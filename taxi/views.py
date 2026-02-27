517a432f7c6245777c89aa8ee3a0f6b2ba231b21
viloker <yarmolykvadym@gmail.com>
2026-02-27 12:59:40 +0100

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 from .models import Driver, Car, Manufacturer
 
@@ -52,3 +53,44 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = Driver
     queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
+
+
+class CarCreateView(generic.CreateView):
+    model = Car
+    success_url = reverse_lazy("taxi:car-list")
+    template_name = "taxi/car_create.html"
+    fields = "__all__"
+
+
+class CarUpdateView(generic.UpdateView):
+    model = Car
+    success_url = reverse_lazy("taxi:car-list")
+    template_name = "taxi/car_create.html"
+    fields = "__all__"
+
+
+class CarDeleteView(generic.DeleteView):
+    model = Car
+
+    success_url = reverse_lazy("taxi:car-list")
+    template_name = "taxi/car_delete.html"
+
+
+class ManufacturerCreateView(generic.CreateView):
+    model = Manufacturer
+    success_url = reverse_lazy("taxi:manufacturer-list")
+    fields = "__all__"
+    template_name = "taxi/manufactory_create.html"
+
+
+class ManufacturerUpdateView(generic.UpdateView):
+    model = Manufacturer
+    success_url = reverse_lazy("taxi:manufacturer-list")
+    fields = "__all__"
+    template_name = "taxi/manufactory_create.html"
+
+
+class ManufacturerDeleteView(generic.DeleteView):
+    model = Manufacturer
+    success_url = reverse_lazy("taxi:manufacturer-list")
+    template_name = "taxi/manufacturer_delete.html"
